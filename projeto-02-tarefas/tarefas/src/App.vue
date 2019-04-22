7043c122e1a34fb98af624d2911085338cc3d7eb
GabrielRodrigues1215 <gabriel.playmovi@gmail.com>
2019-04-22 15:48:32 -0300

    projeto-02-tarefas

---
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Tarefas</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background: #403b4a; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #403b4a,
+    #e7e9bb
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #403b4a, #e7e9bb);
+}
+
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
+</style>
