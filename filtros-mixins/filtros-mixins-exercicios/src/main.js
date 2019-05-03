b3a2c0267f16b856b624b7ab2ae0f3bd7b4a7115
GabrielRodrigues1215 <gabriel.playmovi@gmail.com>
2019-05-03 16:07:25 -0300

    aula220

--- filtros-mixins/filtros-mixins-exercicios/src/main.js
+++ filtros-mixins/filtros-mixins-exercicios/src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('inverter', function (valor) {
+	return valor.split('').reverse().join('')
+})
+
+
+Vue.mixin({
+	created() {
+		console.log('Created - Mixin Global!')
+	}
+})
+
+
 new Vue({
 	render: h => h(App)
 }).$mount('#app')
